cf49fad2a05a65f3f7e72ad38ec7c9d64220ff72
souzjfe <souzajefison@gmail.com>
2021-11-27 15:43:46 -0300

    refactor: removendo codeMirror e adicionando Monaco editor - ♻️

--- src/pages/[title].tsx
+++ src/pages/[title].tsx
@@ -1,21 +1,29 @@
 import React from 'react'
+import 'codemirror/theme/dracula.css'
 import { GetServerSideProps } from 'next'
 import ReactMarkdown from 'react-markdown'
 
+// editor imports
+import CodeMirror from "@uiw/react-codemirror";
+import { markdown, markdownLanguage } from "@codemirror/lang-markdown";
+import { languages } from "@codemirror/language-data";
 
 
-const Post: React.FC = props => {
+
+const Article: React.FC = props => {
   
 
   return (
-    <div></div>
+    <CodeMirror
+    options={{theme:'dracula'}}
+    />
   )
 }
 
 // export const getServerSideProps: GetServerSideProps = async (context) => {
-//   const res = await fetch(`http://localhost:3000/api/post/${context.params.id}`)
+//   const res = await fetch(`http://localhost:3000/api/article/${context.params.id}`)
 //   const data = await res.json()
 //   return { props: { ...data } }
 // }
 
-export default Post
+export default Article
